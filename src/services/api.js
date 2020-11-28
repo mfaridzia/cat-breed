915cb1eb13956ebe9c6b967f661422d3569a5c69
mfaridzia <farid19barcelona@gmail.com>
2020-11-28 09:25:07 +0700

    chore: remove unnecessary code

--- src/services/api.js
+++ src/services/api.js
@@ -1,17 +1,3 @@
-// import axios from 'axios';
-
-// const httpClient = axios.create({
-//   baseURL: process.env.REACT_APP_BASE_URL,
-//   timeout: 5000
-// });
-
-// httpClient.interceptors.request.use(function (config) {
-//     config.headers['x-api-key'] = process.env.REACT_APP_API_KEY;
-//     return config;
-// });
-
-// export default httpClient;
-
 import axios from 'axios';
 
 const http = axios.create ({
@@ -20,8 +6,7 @@ const http = axios.create ({
   headers: {'Content-Type': 'application/json'},
 });
 
-http.interceptors.request.use (
-  function (config) {
+http.interceptors.request.use(function(config) {
     const apiKey = process.env.REACT_APP_API_KEY;
     if (apiKey) config.headers['x-api-key'] = apiKey;
     return config;
