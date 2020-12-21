7a375d51c9c3e54704ecce317efbd928f9a339d2
mfaridzia <muhfaridzia@gmail.com>
2020-12-21 19:53:35 +0700

    style: fix inconsistent code style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { lazy, Suspense } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Container from "./components/Wrapper/Container";
 import Spinner from './components/Loading/Spinner';
 const Home = lazy(() => import('./pages/Home'));
